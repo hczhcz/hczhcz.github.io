ed1db522596c33996c16b4488b80e22bd127dd5d
hcz <h.cz@qq.com>
2014-01-09 16:35:57 +0800

    add abstract and categories

--- scripts/isotope.blog.js
+++ scripts/isotope.blog.js
@@ -6,7 +6,7 @@ $("#posts").post(
     "{{ post.date | date: "%Y-%m-%d" }}",
     "{{ post.title }}",
     "{{ post.url }}",
-    $(""),
+    $("<p />").text("{{ post.abstract }}"),
     [[1, 1], [1, 1], [1, 1], [1, 2], [1, 2], [2, 1], [2, 2]]
 );
 {% endfor %}
