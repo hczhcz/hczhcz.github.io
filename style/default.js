d78a0fce593b992cb84c9ae7b348831dae0ad5d2
hcz <h.cz@qq.com>
2014-01-09 13:42:53 +0800

    add resize event

--- style/default.js
+++ style/default.js
@@ -41,11 +41,29 @@ $(function() {
 
     ib.attr("src", window.ibname);
 
+    var oldw = 0;
+    var fixsize = function() {
+        var neww = Math.max(
+            Math.min(
+                $(window).height() * 2,
+                $(window).width()
+            ), $(window).width() / 2
+        )
+
+        if (oldw !== neww) {
+            ib.animate({
+                width: neww,
+                bottom: - neww / 9
+            });
+            oldw = neww;
+        }
+    }
+
     var gotop = function() {
         if (iblast !== 1) {
             ib
                 .cutfx()
-                .animate({bottom: 0});
+                .animate({bottom: 0}, fixsize);
             iblast = 1;
         }
     }
@@ -54,8 +72,8 @@ $(function() {
         if (iblast !== 2 || ib.queue("fx").length === 0) {
             ib
                 .cutfx()
-                .animate({bottom: - ib.height() / 2})
-                .animate({bottom: - ib.height() / 3});
+                .animate({bottom: - ib.height() / 2}, fixsize)
+                .animate({bottom: - ib.height() / 3}, fixsize);
             iblast = 2;
         }
     }
@@ -70,11 +88,17 @@ $(function() {
         }
     }
 
-    ib.mouseenter(gotop).mouseleave(gobottom);
-    $(window).scroll(gobottom);
+    // Events
+    ib
+        .mouseenter(gotop)
+        .mouseleave(gobottom)
+        .load(function() {
+        // Start up animation
+            gotop();
+            gobottom();
+        });;
 
-    // Start up animation
-    ib.load(function() {
-        ib.mouseenter().mouseleave();
-    });
+    $(window)
+        .scroll(gobottom)
+        .resize(gobottom);
 });
