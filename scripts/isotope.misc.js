d5abe9201c6c45bb86257541d524b7f2ead89145
hcz <h.cz@qq.com>
2014-01-20 23:49:36 +0800

    add rss/atom link

--- scripts/isotope.misc.js
+++ scripts/isotope.misc.js
@@ -10,3 +10,25 @@ $("#posts").post(
         .attr("src", "/icons/GitHub_big.png"),
     [[1, 2]]
 );
+
+$("#posts").post(
+    "",
+    ["RSS"],
+    "",
+    "/rss.xml",
+    $("<img />")
+        .addClass("bigicon")
+        .attr("src", "/icons/Feed_big.png"),
+    [[1, 2]]
+);
+
+$("#posts").post(
+    "",
+    ["Atom"],
+    "",
+    "/atom.xml",
+    $("<img />")
+        .addClass("bigicon")
+        .attr("src", "/icons/Feed_big.png"),
+    [[1, 2]]
+);
