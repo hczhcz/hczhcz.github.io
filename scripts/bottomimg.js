c215956ca0e8cde1c796f06eebc2bf529dd4ec6c
hcz <h.cz@qq.com>
2014-02-02 14:42:22 +0800

    remove delay exec from bottomimg.js

--- scripts/bottomimg.js
+++ scripts/bottomimg.js
@@ -1,154 +1,152 @@
-$(function() {
-    var iblast = 0; // 1->top 2->bottom
+var iblast = 0; // 1->top 2->bottom
 
-    var ib = $("<img />")
-        .addClass("img_bottom")
-        .attr("id", "img_bottom")
-        .attr("alt", "")
-        .appendTo("body");
+var ib = $("<img />")
+    .addClass("img_bottom")
+    .attr("id", "img_bottom")
+    .attr("alt", "")
+    .appendTo("body");
 
-    ib.attr("src", window.ibname);
+ib.attr("src", window.ibname);
 
-    var tbenable = false;
+var tbenable = false;
 
-    var tbupdate = function() {
-        if (
-            $(window).scrollTop() + $(window).height() === $(document).height()
-            &&
-            $(window).scrollTop() > 0
-        ) {
-            tbenable = true;
-        }
-
-        if (tbenable) {
-            tb.cutfx().animate({
-                "height": ib.height()
-            }, 800, "easeOutBounce");
-        } else {
-            tb.cutfx().animate({
-                "height": 0
-            }, 800, "easeOutBounce");
-        }
+var tbupdate = function() {
+    if (
+        $(window).scrollTop() + $(window).height() === $(document).height()
+        &&
+        $(window).scrollTop() > 0
+    ) {
+        tbenable = true;
     }
 
-    var tb = $("<div />")
-        .addClass("div_bottom")
-        .attr("id", "div_bottom")
-        .appendTo("body");
-
-    $("<div />")
-        .addClass("div_bottom_shadow")
-        .attr("id", "div_bottom_shadow")
-        .appendTo(tb);
-
-    var isbuttom = function() {
-        return tbenable || $(window).scrollTop() + $(window).height() > $(document).height() - ib.height();
+    if (tbenable) {
+        tb.cutfx().animate({
+            "height": ib.height()
+        }, 800, "easeOutBounce");
+    } else {
+        tb.cutfx().animate({
+            "height": 0
+        }, 800, "easeOutBounce");
     }
+}
 
-    var oldw = 0;
-    var fixsize = function() {
-        var neww = Math.max(
-            Math.min(
-                $(window).height() * 2,
-                $(window).width()
-            ), $(window).width() / 2
-        );
+var tb = $("<div />")
+    .addClass("div_bottom")
+    .attr("id", "div_bottom")
+    .appendTo("body");
 
-        if (oldw !== neww || (ib.queue("fx").length == 0 && ib.width() !== neww)) {
-            if (isbuttom()) {
-                ib
-                    .cutfx()
-                    .animate({
-                        width: neww,
-                        bottom: 0
-                    });
-            } else {
-                ib
-                    .cutfx()
-                    .animate({
-                        width: neww,
-                        bottom: - neww / 9
-                    });
-            }
-            oldw = neww;
-        }
+$("<div />")
+    .addClass("div_bottom_shadow")
+    .attr("id", "div_bottom_shadow")
+    .appendTo(tb);
 
-        $("body").cutfx().animate({
-            "padding-bottom": ib.height()
-        });
+var isbuttom = function() {
+    return tbenable || $(window).scrollTop() + $(window).height() > $(document).height() - ib.height();
+}
 
-        tbupdate();
-    }
+var oldw = 0;
+var fixsize = function() {
+    var neww = Math.max(
+        Math.min(
+            $(window).height() * 2,
+            $(window).width()
+        ), $(window).width() / 2
+    );
 
-    var gotop = function() {
-        if (iblast !== 1) {
-            ib
-                .cutfx()
-                .animate({bottom: 0}, fixsize);
-            iblast = 1;
-        }
-    }
-
-    var gorun = function() {
-        if (iblast !== 2 || ib.queue("fx").length === 0) {
-            ib
-                .cutfx()
-                .animate({bottom: - ib.height() / 2}, fixsize)
-                .animate({bottom: - ib.height() / 3}, fixsize);
-            iblast = 2;
-        }
-    }
-
-    var gobottom = function() {
+    if (oldw !== neww || (ib.queue("fx").length == 0 && ib.width() !== neww)) {
         if (isbuttom()) {
-            gotop();
+            ib
+                .cutfx()
+                .animate({
+                    width: neww,
+                    bottom: 0
+                });
         } else {
-            gorun();
+            ib
+                .cutfx()
+                .animate({
+                    width: neww,
+                    bottom: - neww / 9
+                });
         }
+        oldw = neww;
     }
 
-    var tboff = function() {
-        tbenable = false;
+    $("body").cutfx().animate({
+        "padding-bottom": ib.height()
+    });
+
+    tbupdate();
+}
+
+var gotop = function() {
+    if (iblast !== 1) {
+        ib
+            .cutfx()
+            .animate({bottom: 0}, fixsize);
+        iblast = 1;
+    }
+}
+
+var gorun = function() {
+    if (iblast !== 2 || ib.queue("fx").length === 0) {
+        ib
+            .cutfx()
+            .animate({bottom: - ib.height() / 2}, fixsize)
+            .animate({bottom: - ib.height() / 3}, fixsize);
+        iblast = 2;
+    }
+}
+
+var gobottom = function() {
+    if (isbuttom()) {
+        gotop();
+    } else {
+        gorun();
+    }
+}
+
+var tboff = function() {
+    tbenable = false;
+    tbupdate();
+    gobottom();
+}
+
+// Events
+ib
+    .mouseenter(gotop)
+    .mouseleave(gobottom)
+    .click(function() {
+        tbenable = true;
         tbupdate();
+    })
+    .load(function() {
+        // Start up animation
+        gotop();
         gobottom();
+    });
+
+tb
+    .mouseleave(tboff);
+
+$("#base")
+    .click(tboff);
+
+$(window)
+    .scroll(tboff)
+    .resize(gobottom);
+
+window.setInterval(fixsize, 1000);
+
+// Skip if shift is held
+var checkshift = function(event) {
+    if (event.shiftKey || event.ctrlKey || event.altKey) {
+        ib.css("pointer-events", "none");
+    } else {
+        ib.css("pointer-events", "auto");
     }
+}
 
-    // Events
-    ib
-        .mouseenter(gotop)
-        .mouseleave(gobottom)
-        .click(function() {
-            tbenable = true;
-            tbupdate();
-        })
-        .load(function() {
-            // Start up animation
-            gotop();
-            gobottom();
-        });
-
-    tb
-        .mouseleave(tboff);
-
-    $("#base")
-        .click(tboff);
-
-    $(window)
-        .scroll(tboff)
-        .resize(gobottom);
-
-    window.setInterval(fixsize, 1000);
-
-    // Skip if shift is held
-    var checkshift = function(event) {
-        if (event.shiftKey || event.ctrlKey || event.altKey) {
-            ib.css("pointer-events", "none");
-        } else {
-            ib.css("pointer-events", "auto");
-        }
-    }
-
-    $(document)
-        .keydown(checkshift)
-        .keyup(checkshift);
-});
+$(document)
+    .keydown(checkshift)
+    .keyup(checkshift);
